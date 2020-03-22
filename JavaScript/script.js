04bcd9e4900bcae7a3e5af3fda6a2e95100a53cd
Nwakusor Mitchell <udemenwak@gmail.com>
2020-03-22 11:53:00 +0100

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,15 @@
+var name = "Mitchell Nwakusor";
+
+var myCourses = ["HTML","CSS","JAVASCRIPT","C#","KOTLIN","UI/UX"];
+
+console.log(name);
+console.log("My courses are: ");
+
+for(let i = 0; i<myCourses.length; i++){
+    console.log(myCourses[i]);
+}
+for(let i = 2; i<=200;i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
